aeabeb1a51e0bc71b46db6f2737cdead8353d22b
Arina M <arenamaralena@yandex.ru>
2025-07-01 22:52:14 +0300

    add service and controller

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -0,0 +1,5 @@
+package ru.kata.spring.boot_security.demo.service;
+
+public interface UserService {
+    public Object getById(long id);
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+    @Override
+    public Object getById(long id) {
+        return null;
+    }
+}
